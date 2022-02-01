b273db423cb2ce57ebae72bbcaf42eccc65568d7
Beatrice Schuster <tbschuster1@gmail.com>
2022-02-01 15:38:54 +0000

    Response function now functional

--- cmd/minimal.cpp
+++ cmd/minimal.cpp
@@ -1,6 +1,6 @@
 #include "command.h"
 #include "dwi/sdeconv/response.h"
-#include "dwi/shells.h"
+// #include "dwi/shells.h"
 
 
 using namespace MR;
@@ -20,36 +20,10 @@ void usage () {
 
 
 
-// Create a class to represent the response
-//    public attributes of the class:
-//      1. Method to retrieve the coeffs at a given b-value
-//      2. Method to query the max harmonic order (that it stores)
-
-class Response{
-    using BValueList = decltype(std::declval<const Eigen::MatrixXd>().col(0));
-
-    public:
-        Eigen::VectorXd retrieve_coeffs(const BValueList bval, const Eigen::Vector3d& dir)
-        {
-            // this method retrieves the SH coefficients for a given b-value
-            Eigen::MatrixXd M, Y;
-            Eigen::VectorXd amplitudes, b;
-
-            // coef_vec = S - M
-            // M[i] = Y[i]
-
-
-            // std::distance(x.begin(), std::min_element(x.begin(), x.end()));
-        }
-
-
-        int max_harmonic()b
-        {
-            
-        }
-};
-
 void run()
 {
+    DWI::SDeconv::Response response(argument[0]);
 
+    VAR(response.lmax());
+    VAR(response.coeffs(3500));
 }
--- src/dwi/sdeconv/response.h
+++ src/dwi/sdeconv/response.h
@@ -4,13 +4,7 @@
 #include "math/ZSH.h"
 #include "math/math.h"
 #include "mrtrix.h"
-
-// Create a class to represent the response
-//    public attributes of the class:
-//      1. Method to retrieve the coeffs at a given b-value
-//      2. Method to query the max harmonic order (that it stores)
-
-// const MR::App::OptionGroup GradImportOptions = DWI::GradImportOptions();
+#include "debug.h"
 
 namespace MR
 {
@@ -20,84 +14,57 @@ namespace MR
     {
       class Response
       {
-        // add method to load matrixes and bvalues that correspond to it; + error handling ()
-
-        // to make sure that bvals.size() = original.coefs.rows() to make sure the size is the same.
-        //        - if it's not the case, we throw an exception: assert it and check for it
-
-        // constructor or method taking a const str as arg; load the data from that;
-        //        - create constructor with the same arguments as the method
-        // get the comments, find the one with shells, strip the shells to get the nrs, parse_float for bvalues vector
 
       public:
-        // template<typename VecType>
-        Eigen::VectorXd retrieve_coeffs(const double bval)
+        Response() {}
+        Response(const std::string &filename) { load(filename); }
+
+        Eigen::VectorXd coeffs(const double bval)
         {
-          Eigen::VectorXd coefs(original_coefs.cols());
+          if (bval < original_bvals[0])
+            throw Exception("bvalue out of bounds");
+
+          if (bval >= original_bvals.back())
+            return original_coefs.row(original_coefs.rows() - 1);
 
           int i = 0;
-          vector<double> dist_up, dist_down;
-
-          while (bval < original_bvals[i])
+          while (bval > original_bvals[i + 1])
             i++;
 
-          // Linear interpolation
           float ratio = (bval - original_bvals[i]) / (original_bvals[i + 1] - original_bvals[i]);
-          coefs = (1.0 - ratio) * original_coefs.row(i) + ratio * original_coefs.row(i + 1);
-
-          if (i == original_coefs.cols())
-            throw Exception("Iterator is out of boundaries");
-
-          // if (!(original_coefs.coeffs().minCoeff() < bval < original_coefs.coeffs().maxCoeff()))
-          //   throw Exception("b-value is out of boundaries, cannot interpolate");
-
-          // // calculate difference between bval and elements of original_bvals
-          // if (bval == i)
-          //   return bval;
-          // else if (i > bval)
-          //   dist_up.push_back(abs(i - bval));
-          // else
-          //   dist_down.push_back(abs(i - bval));
-
-          // // find bval neighbours
-          // float up_bound = find(dist_up.begin(), dist_up.end(), min_element(dist_up.begin(), dist_up.end())),
-          //       low_bound = find(dist_down.begin(), dist_down.end(), min_element(dist_down.begin(), dist_down.end()));
-
-          // // interpolate
-          // float ratio = (bval - low_bound) / (up_bound - low_bound);
-          // coefs = (1.0 - ratio) * original_coefs.row(low_bound) + original_coefs.row(up_bound) * ratio;
-
-          return coefs;
+          return (1.0 - ratio) * original_coefs.row(i) + ratio * original_coefs.row(i + 1);
         }
 
-        int lmax() const
-        {
+        int lmax() const { return Math::ZSH::LforN(original_coefs.cols()); }
 
-          return Math::ZSH::LforN(original_coefs.cols());
-        }
+        const Eigen::VectorXd &bvalues() const { return original_bvals; }
 
         // LOAD MATRIX
-        Eigen::MatrixXd load_matrix(const std::string &filename, vector<std::string>* comments)
+        void load(const std::string &filename)
         {
-          auto vec = load_matrix_2D_vector(filename, comments);
-          Eigen::MatrixXd M(vec.size(), vec[0].size());
+          vector<std::string> comments;
+          auto vec = load_matrix_2D_vector(filename, &comments);
+
+          //load bvalues
+          for (const auto &comment : comments)
+          {
+            auto n = comment.rfind("Shells:");
+            if (n != comment.npos)
+              original_bvals = parse_floats(comment.substr(n + 7));
+          }
+
+          if (original_bvals.size() != vec.size())
+            throw Exception("Number of b-values does not match the number of rows in response");
+
+          if (!std::is_sorted(original_bvals.begin(), original_bvals.end()))
+            throw Exception("B-values are not sorted in ascending order");
+
+          original_coefs.resize(vec.size(), vec[0].size());
 
           for (ssize_t r = 0; r < vec.size(); r++)
-            for (ssize_t c = 0; c < vec[0].size(); r++)
-              M(r, c) = vec[r][c];
-
-          return M;
+            for (ssize_t c = 0; c < vec[0].size(); c++)
+              original_coefs(r, c) = vec[r][c];
         }
-
-        
-        // const bool shell_bvalues = App::get_options("shell_bvalues").size();
-        // const bool shell_sizes = App::get_options("shell_sizes").size();
-        // const bool shell_indices = App::get_options("shell_indices").size();
-
-        // Eigen::MatrixXd grad = DWI::get_DW_scheme(header, DWI::get_cmdline_bvalue_scaling_behaviour());
-
-        // if (shell_bvalues || shell_sizes || shell_indices)
-        //   print_shells(grad, shell_bvalues, shell_sizes, shell_indices);
 
       private:
         Eigen::MatrixXd original_coefs;
