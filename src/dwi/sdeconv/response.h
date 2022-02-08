98f8f895975c20c8f9423b88b2f3b6a2b18ea78b
Beatrice Schuster <tbschuster1@gmail.com>
2022-02-08 17:24:11 +0100

    dwi2fod: first attempt at arbitrary dwi encoding fit

--- src/dwi/sdeconv/response.h
+++ src/dwi/sdeconv/response.h
@@ -18,6 +18,7 @@ namespace MR
       public:
         Response() {}
         Response(const std::string &filename) { load(filename); }
+        Response(Response &&other) = default;
 
         Eigen::VectorXd coeffs(const double bval)
         {
@@ -25,19 +26,21 @@ namespace MR
             throw Exception("bvalue out of bounds");
 
           if (bval >= original_bvals.back())
-            return original_coefs.row(original_coefs.rows() - 1);
+            return original_coeffs.row(original_coeffs.rows() - 1);
 
           int i = 0;
           while (bval > original_bvals[i + 1])
             i++;
 
           float ratio = (bval - original_bvals[i]) / (original_bvals[i + 1] - original_bvals[i]);
-          return (1.0 - ratio) * original_coefs.row(i) + ratio * original_coefs.row(i + 1);
+          return (1.0 - ratio) * original_coeffs.row(i) + ratio * original_coeffs.row(i + 1);
         }
 
-        int lmax() const { return Math::ZSH::LforN(original_coefs.cols()); }
+        int lmax() const { return Math::ZSH::LforN(original_coeffs.cols()); }
 
-        const Eigen::VectorXd &bvalues() const { return original_bvals; }
+        const vector<double> &bvalues() const { return original_bvals; }
+
+        // int size() const { return original_bvals.size(); }
 
         // LOAD MATRIX
         void load(const std::string &filename)
@@ -59,15 +62,15 @@ namespace MR
           if (!std::is_sorted(original_bvals.begin(), original_bvals.end()))
             throw Exception("B-values are not sorted in ascending order");
 
-          original_coefs.resize(vec.size(), vec[0].size());
+          original_coeffs.resize(vec.size(), vec[0].size());
 
           for (ssize_t r = 0; r < vec.size(); r++)
             for (ssize_t c = 0; c < vec[0].size(); c++)
-              original_coefs(r, c) = vec[r][c];
+              original_coeffs(r, c) = vec[r][c];
         }
 
       private:
-        Eigen::MatrixXd original_coefs;
+        Eigen::MatrixXd original_coeffs;
         vector<double> original_bvals;
       };
     }
